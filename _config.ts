7016ed11d61b6f6455967d3b3ac998ad20e48c85
maroayman <marwanayman.shawky@gmail.com>
2026-01-17 12:02:28 +0200

    Cleanup: Remove redundant pagination files, unused terser plugin, and fix config

--- _config.ts
+++ _config.ts
@@ -1,7 +1,6 @@
 import lume from "lume/mod.ts";
 import vento from "lume/plugins/vento.ts";
 import postcss from "lume/plugins/postcss.ts";
-import terser from "lume/plugins/terser.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 import minifyHTML from "lume/plugins/minify_html.ts";
 import date from "lume/plugins/date.ts";
@@ -9,7 +8,7 @@ import date from "lume/plugins/date.ts";
 const site = lume({
     src: "./src",
     dest: "./dist",
-    location: new URL("https://yourportfolio.com"),
+    location: new URL("https://maroayman.vercel.app"),
 });
 
 // Template engine
@@ -20,14 +19,12 @@ site.use(date());
 
 // Assets
 site.add("styles");
-site.add("scripts");
 site.add("assets");
 site.add("public", ".");
 site.add([".pdf"]);
 
 // Optimizations
 site.use(postcss());
-site.use(terser());
 site.use(sitemap());
 site.use(minifyHTML());
 
